49a346d1707b0c1caa53b1a5c570aa4407d5b334
kucmoving <kuc.moving@gmail.com>
2022-07-15 10:40:15 +0100

    TripClient(Deploy)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl:"https://localhost:7141/api/"
+    apiUrl:"https://tripuppiesapi.azurewebsites.net/api/"
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    apiUrl : "https://localhost:7141/api/"
+    apiUrl : "https://tripuppiesapi.azurewebsites.net/api/"
+    //apiUrl: "https://localhost:7141/api/"
   };
 
 
